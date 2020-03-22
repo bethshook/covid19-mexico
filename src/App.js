2d9fe4c01a3c341a5d7abe2083b28b95b8eaa2d0
Beth Shook <beth.shook@gmail.com>
2020-03-21 23:06:00 -0600

    Embed Google map

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import './App.css';
 import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+// const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 class SimpleMap extends Component {
   static defaultProps = {
@@ -15,19 +14,28 @@ class SimpleMap extends Component {
 
   render() {
     return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyC3ETM1XmSXWdP0PMaHa7pW684iPdEd8OQ' }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text="My Marker"
-          />
-        </GoogleMapReact>
+      <div>
+      <header className="App-header">
+          <h1>Apoyo Mutuo - Roma Norte</h1>
+          <h3>Recursos</h3>
+          <ul>
+            <li>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </li>
+          </ul>
+        </header>
+        <div style={{ width: '100%', display: 'flex',  justifyContent:'center', alignItems:'center' }}>
+          <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1F2SSHTJxy8znHus4qOosLcqas__A6j4j" width="640" height="480">
+          </iframe>
+
+        </div>
       </div>
     );
   }
